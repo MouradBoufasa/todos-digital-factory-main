cbda54a6b6856dde549c6a386e8f46770d57e676
Boufasa Mourad <100412477+MouradBoufasa@users.noreply.github.com>
2023-03-23 10:33:19 +0100

    initialize react project with vite

---
+++ todos/src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+function App() {
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
+
+export default App;
